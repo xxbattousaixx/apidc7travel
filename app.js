6fa7ce5a62a9d389dcacabe6cd4d1c84a890b5c9
xxbattousaixx <dcarrassi@outlook.com>
2023-04-28 07:34:29 -0400

    newway

--- app.js
+++ app.js
@@ -1,27 +1,19 @@
 // app.js
-
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
 const app = express();
 require('dotenv').config();
-const mongoose = require('mongoose');
-  const db = "mongodb+srv://dcarrassi:8701423@cluster0.7aqzmi1.mongodb.net/?retryWrites=true&w=majority";
-  
-  const connectDB = async () => {
-    try {
-      mongoose.set('strictQuery', true);
-      await mongoose.connect(db);
-  
-      console.log('MongoDB is Connected...');
-    } catch (err) {
-      console.error(err.message);
-      process.exit(1);
-    }
-  };
+const port = 3100;
+const uri = "mongodb+srv://dcarrassi:8701423@cluster0.7aqzmi1.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(uri);
+const connection = mongoose.connection;
+connection.once('open',()=>{console.log('mongo DB success');
+});
 // routes
 const tripsRouter = require('./routes/api/trips');
 // Connect Database
-connectDB();
 app.use('/images', express.static('images'));
 // cors
 app.use(cors({ origin: true, credentials: true }));
@@ -33,6 +25,5 @@ app.get('/api/trips', (req, res) => res.send('Hello world!'));
 // use Routes
 app.use('/', tripsRouter);
 
-const port = 3100;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
