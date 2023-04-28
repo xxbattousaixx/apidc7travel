4ddfac6d81be3a0796d88fc029a72e41f25c2ba5
xxbattousaixx <dcarrassi@outlook.com>
2023-04-28 07:29:12 -0400

    ok

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 const app = express();
 require('dotenv').config();
+const mongoose = require('mongoose');
+  const db = "mongodb+srv://dcarrassi:8701423@cluster0.7aqzmi1.mongodb.net/?retryWrites=true&w=majority";
+  
+  const connectDB = async () => {
+    try {
+      mongoose.set('strictQuery', true);
+      await mongoose.connect(db);
+  
+      console.log('MongoDB is Connected...');
+    } catch (err) {
+      console.error(err.message);
+      process.exit(1);
+    }
+  };
 // routes
 const tripsRouter = require('./routes/api/trips');
 // Connect Database
