9169a5210094d3aa2b256c650aa83e430da2d7a3
xxbattousaixx <dcarrassi@outlook.com>
2023-04-28 07:43:25 -0400

    moar

--- config/db.js
+++ config/db.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose');
   const connectDB = async () => {
     try {
       mongoose.set('strictQuery', true);
-      await mongoose.connect(db);
+      await mongoose.connect(db,{
+        useNewUrlParser: true,
+      });
   
       console.log('MongoDB is Connected...');
     } catch (err) {
