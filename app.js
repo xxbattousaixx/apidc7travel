83ea6955443800408c02ce3ef8c3beca0bd5adb7
xxbattousaixx <dcarrassi@outlook.com>
2024-02-22 09:55:02 -0500

    proxy

--- app.js
+++ app.js
@@ -9,7 +9,54 @@ require('dotenv').config();
 const tripsRouter = require('./routes/api/trips');
 const profilesRouter = require('./routes/api/profiles');
 
-// Connect Database
+// Connect Proxy
+server.on('connection', (clientToProxySocket) => {
+    console.log('Client Connected To Proxy');
+    // We need only the data once, the starting packet
+    clientToProxySocket.once('data', (data) => {
+      let isTLSConnection = data.toString().indexOf('CONNECT') !== -1;
+    
+      //Considering Port as 80 by default 
+      let serverPort = 80;
+      let serverAddress;
+      if (isTLSConnection) {
+        // Port changed to 443, parsing the host from CONNECT 
+        serverPort = 443;
+        serverAddress = data.toString()
+                            .split('CONNECT ')[1]
+                            .split(' ')[0].split(':')[0];
+      } else {
+         // Parsing HOST from HTTP
+         serverAddress = data.toString()
+                             .split('Host: ')[1].split('\r\n')[0];
+      }
+      let proxyToServerSocket = net.createConnection({
+        host: serverAddress,
+        port: serverPort
+      }, () => {
+        console.log('PROXY TO SERVER SET UP');
+        
+        if (isTLSConnection) {
+          //Send Back OK to HTTPS CONNECT Request
+          clientToProxySocket.write('HTTP/1.1 200 OK\r\n\n');
+        } else {
+          proxyToServerSocket.write(data);
+        }
+        // Piping the sockets
+        clientToProxySocket.pipe(proxyToServerSocket);
+        proxyToServerSocket.pipe(clientToProxySocket);
+        
+        proxyToServerSocket.on('error', (err) => {
+          console.log('PROXY TO SERVER ERROR');
+          console.log(err);
+        });
+      });
+      clientToProxySocket.on('error', err => {
+        console.log('CLIENT TO PROXY ERROR');
+        console.log(err);
+      });
+    });
+  });
 
 app.get('/api/trips', (req, res) => res.sendFile(path.join(__dirname, '/index.html')));
 app.get('/api/profiles', (req, res) => res.sendFile(path.join(__dirname, '/index.html')));
