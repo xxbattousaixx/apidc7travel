346771e79ce8e538017d545ec718fc53d921f617
xxbattousaixx <dcarrassi@outlook.com>
2024-02-26 11:28:20 -0500

    f

--- routes/api/protectedRoute.js
+++ routes/api/protectedRoute.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const verifyToken = require('../middleware/authMiddleware');
+const verifyToken = require('../../middleware/authMiddleware.js');
 // Protected route
 router.get('/', verifyToken, (req, res) => {
 res.status(200).json({ message: 'Protected route accessed' });
