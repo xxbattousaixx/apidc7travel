55d278c8326cbeea2d84b66bd827e83136bd9746
xxbattousaixx <dcarrassi@outlook.com>
2024-02-29 01:56:34 -0500

    ..

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
  username: { type: String, unique: true, required: true },
  password: { type: String, required: true },
  active: { type: String, required: true, default: false  },
- id:{type:Number, required:true}
+ id:{type:Number, required:true, default:''}
  
  });
 module.exports = mongoose.model('User', userSchema);
